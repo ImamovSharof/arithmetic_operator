ee3ad619c5cf06683d337a00d9c0f05f3f715e70
sharof <sharofimamov3@gmail.com>
2022-07-22 10:51:25 +0500

    009

--- bin/arithmetic_operator09.dart
+++ bin/arithmetic_operator09.dart
@@ -10,4 +10,12 @@ Find the expression value assign to 'answer'. a+b - cd .
 */
 void main() {
   //write your code here
+  double a, b, c, javob, d, z;
+  a=8;
+  b=3;
+  c=2;
+  d=4;
+  z=a+b-c*d;
+  javob=z;
+  print(javob);  
 }
