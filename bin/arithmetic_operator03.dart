cc9cdf2b063ec128dca29ab395eb9e51268ecbb4
sharof <sharofimamov3@gmail.com>
2022-07-22 09:57:50 +0500

    002

--- bin/arithmetic_operator03.dart
+++ bin/arithmetic_operator03.dart
@@ -8,4 +8,10 @@ find the difference of a and b then assign to "answer"
 */
 void main() {
   //write your code here
+  int a, b, answer, z;
+  a=6;
+  b=2;
+  z=a-b;
+  answer=z;
+  print(answer);
 }
